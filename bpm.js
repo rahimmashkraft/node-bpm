98c41249be3e51d89bb1c5c4ccb3db494d264c66
rahim.zulfiqar <rahim.zulfiqar@sdsoltechnologies.com>
2022-06-22 14:16:44 +0500

    change bpm code

--- bpm.js
+++ bpm.js
@@ -1,8 +1,10 @@
 /**
- * Calculate BPM
+ * Calculate BPM 
+ * Main code by bahamas10 on github 
+ * edits by David Gooch Gooch12013 on github
  */
 
-if (typeof exports !== 'undefined') {
+ if (typeof exports !== 'undefined') {
   module.exports = BPM;
   module.exports.BPM = BPM;
 }
@@ -22,17 +24,37 @@ BPM.prototype.tap = function() {
   // ignore the first tap
   if (this.old_ts) {
     var ms = this.ts - this.old_ts;
+    
+
+    //adds a reset if the user does not tap than 8 seconds apart 
+    if (ms > 8000) { 
+      this.count = 0;
+      this.ts = 0;
+      this.old_ts = 0;
+      this.first_ts = 0;
+    }
+    
+    //adds a reset if the user taps more than 16 times in a row
+    if (this.count >= 16){
+      this.count = 0;
+      this.ts = 0;
+      this.old_ts = 0;
+      this.first_ts = 0;
+    }
 
     var avg = 60000 * this.count / (this.ts - this.first_ts);
-
-    ret.avg = avg;
+   
+    ret.avg = avg
     ret.ms = ms;
+    
   }
 
   ret.count = ++this.count;
 
   // Store the old timestamp
   this.old_ts = this.ts;
+
+ 
   return ret;
 };
 
@@ -41,4 +63,7 @@ BPM.prototype.reset = function() {
   this.ts = 0;
   this.old_ts = 0;
   this.first_ts = 0;
+  
+  return ret;
 };
+
